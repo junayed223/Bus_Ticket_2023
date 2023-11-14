76d43c673d7461c12391e704fbaf0ef2d3d36eac
Junayed Bin Karim <113838887+junayed223@users.noreply.github.com>
2023-11-14 23:22:57 +0600

    Add files via upload

---
+++ Login_info.java
@@ -0,0 +1,20 @@
+public class Login_info {
+    private String user_name = "Junayed";
+    private String pass = "123";
+
+    public String getUser_name() {
+        return user_name;
+    }
+
+    public void setUser_name(String user_name) {
+        this.user_name = user_name;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+}
